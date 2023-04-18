b09499a0c404be9e750232c76eba5490110c356f
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-18 17:58:33 -0600

    BookPage details

--- src/components/pages/BookPage/BookPage.tsx
+++ src/components/pages/BookPage/BookPage.tsx
@@ -1,24 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Book } from "../../../types/Books";
+import books from "../../../mocks/books.json";
 
 export const BookPage = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  console.log(id);
+
+  const book = books.find((book) => book.id === parseInt(id!)) as Book;
 
   return (
     <div className="bookPage">
       <section className="arrow-back">
-        <a href="">Atras</a>
+        <button onClick={() => navigate(-1)}>Atras</button>
       </section>
       <section className="bookPage--portrait">
-        <img src="" alt="" />
+        <img src={book.img} alt={book.title} />
         <div className="bookPage--titles">
-          <h1></h1>
-          <h2></h2>
+          <h1>{book.title}</h1>
+          <h2>{book.author}</h2>
         </div>
       </section>
-      <p className="bookPage--description"></p>
+      <p className="bookPage--description">{book.description}</p>
       <button className="primaryButton">Leer ahora</button>
-      <section className="other-books"></section>
     </div>
   );
 };
--- src/types/Books.ts
+++ src/types/Books.ts
@@ -4,4 +4,5 @@ export interface Book {
   title: string;
   author: string;
   area: string;
+  description: string;
 }
