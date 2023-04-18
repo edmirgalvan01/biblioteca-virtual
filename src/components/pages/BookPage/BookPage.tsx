1c692411cdfaae31497e7eb9d6709ee2ec26c19d
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-18 17:45:52 -0600

    BookPage semantic html

--- src/components/pages/BookPage/BookPage.tsx
+++ src/components/pages/BookPage/BookPage.tsx
@@ -4,5 +4,21 @@ export const BookPage = () => {
   const { id } = useParams();
   console.log(id);
 
-  return <div>BookPage</div>;
+  return (
+    <div className="bookPage">
+      <div className="arrow-back">
+        <a href="">Atras</a>
+      </div>
+      <div className="bookPage--portrait">
+        <img src="" alt="" />
+        <div className="bookPage--titles">
+          <h1></h1>
+          <h2></h2>
+        </div>
+      </div>
+      <p className="bookPage--description"></p>
+      <button className="primaryButton">Leer ahora</button>
+      <div className="other-books"></div>
+    </div>
+  );
 };
