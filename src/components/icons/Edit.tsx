87428617fbf014d1d71875e8a2f0c3307436b6ad
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-19 08:09:22 -0600

    ProfilePage semantic HTML

---
+++ src/components/icons/Edit.tsx
@@ -0,0 +1,28 @@
+export const Edit = () => {
+  return (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.2599 4.09997L5.04985 12.79C4.73985 13.12 4.43985 13.77 4.37985 14.22L4.00985 17.46C3.87985 18.63 4.71985 19.43 5.87985 19.23L9.09985 18.68C9.54985 18.6 10.1799 18.27 10.4899 17.93L18.6999 9.23997C20.1199 7.73997 20.7599 6.02997 18.5499 3.93997C16.3499 1.86997 14.6799 2.59997 13.2599 4.09997Z"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.89 5.55005C12.0996 6.8909 12.7486 8.12418 13.735 9.0563C14.7214 9.98841 15.9894 10.5666 17.34 10.7M3 22.5H21"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
--- src/components/pages/ProfilePage/ProfilePage.tsx
+++ src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,33 @@
+import { PrimaryButton } from "../../Buttons/Buttons";
+import { ArrowBack } from "../../icons/ArrowBack";
+import { Edit } from "../../icons/Edit";
+
 export const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="profilePage">
+      <ArrowBack />
+      <section className="profilePage--section">
+        <div className="information">
+          <h4>Nombre completo</h4>
+          <input type="text" value="Edmir Galvan Vazquez" />
+        </div>
+        <Edit />
+      </section>
+      <section className="profilePage--section">
+        <div className="information">
+          <h4>Correo electrónico</h4>
+          <input type="email" value="edmirgalvaz@gmail.com" />
+        </div>
+        <Edit />
+      </section>
+      <section className="profilePage--section">
+        <div className="information">
+          <h4>Contraseña</h4>
+          <input type="password" value="123456" />
+        </div>
+        <Edit />
+      </section>
+      <PrimaryButton>Subir un nuevo libro</PrimaryButton>
+    </div>
+  );
 };
