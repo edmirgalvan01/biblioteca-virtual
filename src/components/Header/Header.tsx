2a83f335dfcce08da3b3dadd9a1f5648a0d44933
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-19 08:00:06 -0600

    Header created

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Profile } from "../icons/Profile";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1>Inicio</h1>
+      <Profile />
+    </header>
+  );
+};
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -4,13 +4,14 @@ import database from "../../../mocks/books.json";
 import { Book } from "../../../types/Books";
 import { Filters } from "../../Filters/Filters";
 import { ListOfBooks } from "../../ListOfBooks/ListOfBooks";
+import { Header } from "../../Header/Header";
 
 export const Home = () => {
   const [books, setBooks] = useState<Array<Book>>(database);
 
   return (
     <div className="home">
-      <h1>Inicio</h1>
+      <Header />
       <Filters books={database} setBooks={setBooks} />
       <ListOfBooks books={books} />
     </div>
