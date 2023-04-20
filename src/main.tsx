54690e7ad845c7c1d4b69cc736e7f4fc7da33205
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-20 16:37:58 -0600

    Files to register-login flow created

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./components/pages/Home/Home";
 import { BookPage } from "./components/pages/BookPage/BookPage";
 import { ProfilePage } from "./components/pages/ProfilePage/ProfilePage";
+import { LoginPage } from "./components/pages/LoginPage/LoginPage";
+import { WelcomePage } from "./components/pages/WelcomePage/WelcomePage";
+import { RegisterPage } from "./components/pages/RegisterPage/RegisterPage";
+
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <Home />,
   },
   {
@@ -18,6 +22,18 @@ const router = createBrowserRouter([
   {
     path: "/profile",
     element: <ProfilePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/",
+    element: <WelcomePage />,
   },
 ]);
 
