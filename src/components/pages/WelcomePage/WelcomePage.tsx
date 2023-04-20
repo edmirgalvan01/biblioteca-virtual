97aecca1f0a7475f53d749015cdeb01a38bb6b6e
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-20 16:47:42 -0600

    WelcomePage created

--- src/components/pages/WelcomePage/WelcomePage.tsx
+++ src/components/pages/WelcomePage/WelcomePage.tsx
@@ -1,3 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { PrimaryButton, SecondaryButton } from "../../Buttons/Buttons";
+import "./WelcomePage.css";
+
 export const WelcomePage = () => {
-  return <div>WelcomePage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="welcomePage">
+      <div className="welcomePage--titles">
+        <p>Bienvenido/a</p>
+        <h1>Biblioteca virtual</h1>
+        <h3>CBTis No. 78</h3>
+      </div>
+      <div className="welcomePage--buttons">
+        <PrimaryButton onClick={() => navigate("/register")}>
+          Registrarse
+        </PrimaryButton>
+        <SecondaryButton onClick={() => navigate("/login")}>
+          Iniciar sesión
+        </SecondaryButton>
+      </div>
+    </section>
+  );
 };
