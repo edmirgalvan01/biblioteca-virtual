14d2f559cbd1d368b190ddbea688202118dc0071
Edmir Galvan <edmirgalvaz@gmail.com>
2023-04-21 07:12:43 -0600

    Error message to LogiPage

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
-import { BackButton } from "../../BackButton";
 import { PrimaryButton } from "../../Buttons/Buttons";
+import { AuthError } from "@supabase/supabase-js";
 import { InputField } from "../../Fields/Fields";
-import "./LoginPage.css";
 import { useUser } from "../../../hooks/useUser";
+import { BackButton } from "../../BackButton";
+import { useState } from "react";
+import "./LoginPage.css";
 
 export const LoginPage = () => {
   const { signInUser } = useUser();
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<AuthError>();
 
   const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    resetForm();
-    signInUser(email, password);
-  };
 
-  const resetForm = () => {
-    setEmail("");
-    setPassword("");
+    signInUser(email, password).then(({ data, error }) => {
+      if (error) setError(error);
+    });
   };
 
   return (
@@ -47,6 +46,7 @@ export const LoginPage = () => {
           }}
           value={password}
         />
+        {error && <p className="errorMessage">Credenciales inválidas</p>}
         <PrimaryButton type="submit">Iniciar sesión</PrimaryButton>
       </form>
     </div>
