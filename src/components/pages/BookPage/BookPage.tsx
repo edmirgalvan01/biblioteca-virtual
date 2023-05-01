d2a6a13bffd4cfa15829f48115da8f3306427501
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-01 12:13:14 -0600

    BookPage error fixed

--- src/components/pages/BookPage/BookPage.tsx
+++ src/components/pages/BookPage/BookPage.tsx
@@ -1,28 +1,30 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { Book } from "../../../types/Books";
-import books from "../../../mocks/books.json";
-import "./BookPage.css";
-import { PrimaryButton } from "../../Buttons/Buttons";
+import { useParams } from "react-router-dom";
+
 import { ListOfBooksWithTitle } from "../../ListOfBooks/ListOfBooks";
+import { PrimaryButton } from "../../Buttons/Buttons";
 import { BackButton } from "../../BackButton";
 
+import { useGetBooks } from "../../../hooks/useGetBooks";
+
+import "./BookPage.css";
+
 export const BookPage = () => {
-  const navigate = useNavigate();
+  const { books } = useGetBooks();
   const { id } = useParams();
 
-  const book = books.find((book) => book.id === parseInt(id!)) as Book;
+  const book = books.find((book) => book.id === parseInt(id!));
 
   return (
     <div className="bookPage">
       <BackButton />
       <section className="bookPage--portrait">
-        <img src={book.img} alt={book.title} />
+        <img src={book?.img} alt={book?.title} />
         <div className="bookPage--titles">
-          <h1>{book.title}</h1>
-          <h2>{book.author}</h2>
+          <h1>{book?.title}</h1>
+          <h2>{book?.author}</h2>
         </div>
       </section>
-      <p className="bookPage--description">{book.description}</p>
+      <p className="bookPage--description">{book?.description}</p>
       <PrimaryButton>Leer ahora</PrimaryButton>
       <ListOfBooksWithTitle
         title="Otros libros"
