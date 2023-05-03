1fadf79290a1e7cce570edff4d8b0513b19ed748
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-02 18:37:29 -0600

    Fixed

--- src/hooks/useSignInUser.ts
+++ src/hooks/useSignInUser.ts
@@ -1,25 +1,21 @@
 import { signInUser } from "../services/user.service";
-import { AuthError } from "@supabase/supabase-js";
 import { useNavigate } from "react-router-dom";
 import { ResponseType } from "../types/Users";
-import { useState } from "react";
 
 export const useSignInUser = () => {
   const navigate = useNavigate();
-  const [responseError, setResponseError] = useState<AuthError | null>();
 
   const loggedIn = async (
     email: string,
     password: string
   ): Promise<ResponseType> => {
     const { error } = await signInUser(email, password);
-    setResponseError(error);
 
-    if (responseError) {
+    if (error) {
       return {
         success: false,
         errors: {
-          errorSignIn: responseError,
+          errorSignIn: error,
         },
       };
     } else {
