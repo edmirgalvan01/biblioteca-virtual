f678c77f07d2fc6bcd2884a22e6015cbb5da7dc8
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-08 11:18:41 -0600

    If the book have been marked as favorite already

--- src/hooks/useIsFavoriteBook.ts
+++ src/hooks/useIsFavoriteBook.ts
@@ -6,7 +6,9 @@ export const useIsFavoriteBook = (bookId: number) => {
   const { favoriteData } = useGetFavoriteData();
 
   useEffect(() => {
+    //Si alguno coincide con el bookId significa que si fue marcado como favorito
     const response = favoriteData.some((book) => book.book_id === bookId);
+    console.log({ favoriteData, response });
 
     setIsFavorite(response);
   }, [favoriteData]);
--- src/hooks/useMarkBookAsFavorite.ts
+++ src/hooks/useMarkBookAsFavorite.ts
@@ -1,20 +1,32 @@
 import { markBookAsFavorite } from "../services/book.service";
 import { BookAsFavorite } from "../types/Books";
 import { ResponseType } from "../types/Users";
+import { useGetFavoriteData } from "./useGetFavoriteData";
 
 export const useMarkBookAsFavorite = () => {
+  const { favoriteData } = useGetFavoriteData();
+
   const markAsFavorite = async (
     data: BookAsFavorite
   ): Promise<ResponseType> => {
-    const error = await markBookAsFavorite(data);
+    const isMarkedFavorite = favoriteData.some(
+      (book) => book.book_id === data.book_id
+    );
 
-    if (error) {
-      return {
-        success: false,
-        errors: {
-          errorMarkAsFavorite: error,
-        },
-      };
+    //Si el libro no existe en favoritos
+    if (!isMarkedFavorite) {
+      const error = await markBookAsFavorite(data);
+
+      if (error) {
+        return {
+          success: false,
+          errors: {
+            errorMarkAsFavorite: error,
+          },
+        };
+      } else {
+        return { success: true };
+      }
     } else {
       return { success: true };
     }
