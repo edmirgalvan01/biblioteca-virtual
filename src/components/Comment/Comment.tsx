30a90f24cfed497cb740b999e66ff08873df133b
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-10 12:49:52 -0600

    Comment semantic created

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,5 +1,30 @@
 import "./Comment.css";
+import { BiLike, BiPencil, BiTrash } from "react-icons/bi";
 
 export const Comment = () => {
-  return <div>Comment</div>;
+  return (
+    <article className="comment">
+      <header>
+        <div className="comment--user-profile">
+          <p>R</p>
+        </div>
+        <div className="comment--header--info">
+          <div className="comment--user-info">
+            <p className="comment--user-name">Ralph Edwards</p>
+            <p className="comment--date">Mayo 2023</p>
+          </div>
+          <div className="comment--buttons">
+            <BiLike size={25} color="#A9A9A9" />
+            <BiPencil size={25} color="#A9A9A9" />
+            <BiTrash size={25} color="#A9A9A9" />
+          </div>
+        </div>
+      </header>
+      <p className="comment--content">
+        Lorem ipsum dolor sit amet consectetur. Adipiscing consectetur non
+        elementum risus orci eu sagittis mauris lacinia. Bibendum adipiscing
+        urna quis.
+      </p>
+    </article>
+  );
 };
--- src/components/ListOfComments/ListOfComments.tsx
+++ src/components/ListOfComments/ListOfComments.tsx
@@ -1,5 +1,10 @@
+import { Comment } from "../Comment/Comment";
 import "./ListOfComments.css";
 
 export const ListOfComments = () => {
-  return <div className="listOfComments"></div>;
+  return (
+    <div className="listOfComments">
+      <Comment />
+    </div>
+  );
 };
