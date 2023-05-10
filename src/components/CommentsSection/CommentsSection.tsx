4d7cbccb411e0edafe008320fb0120511d902118
Edmir Galvan <edmirgalvaz@gmail.com>
2023-05-10 13:06:39 -0600

    CommentsSection change design

--- src/components/CommentsSection/CommentsSection.tsx
+++ src/components/CommentsSection/CommentsSection.tsx
@@ -1,13 +1,23 @@
-import { PrimaryButton } from "../Buttons/Buttons";
+import { useState } from "react";
 import { ListOfComments } from "../ListOfComments/ListOfComments";
 import "./CommentsSection.css";
 
 export const CommentsSection = () => {
+  const [newComment, setNewComment] = useState<string>("");
+
   return (
     <div className="commentsSection">
       <h2>Comentarios</h2>
+      <form className="commentsSection--form">
+        <input
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Escribe tu comentario..."
+          name="comment"
+          value={newComment}
+        />
+        <button type="submit">Guardar</button>
+      </form>
       <ListOfComments />
-      <PrimaryButton>Nuevo comentario</PrimaryButton>
     </div>
   );
 };
